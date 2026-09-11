4903f506ab45ba96a328ad04dd1129518fcf8c75
Priyanshu Mishra <ofviewp0@gmail.com>
2026-09-11 19:08:35 +0530

    Add assignment problem 3 solution

---
+++ Step_semester_3/src/main/java/access_modifiers_encapsulation/assigment_problems/BookCopyCirculationGuard.java
@@ -0,0 +1,31 @@
+public class BookCopyCirculationGuard {
+}
+
+class BookInventory {
+    private final int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+        if (copiesTotal <= 0 || copiesTotal > 500) {
+            throw new IllegalArgumentException("Invalid copiesTotal");
+        }
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    public void checkIn() {
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
